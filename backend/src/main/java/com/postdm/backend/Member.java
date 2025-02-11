d2d88ba78a767f40130f5f2af764e41949101ff1
Etwashoeren <sungmin0609@nate.com>
2025-02-12 00:31:50 +0900

    feat. Member 엔티티 생성

---
+++ backend/src/main/java/com/postdm/backend/Member.java
@@ -0,0 +1,17 @@
+package com.postdm.backend;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String username;
+
+    private String password;
+}

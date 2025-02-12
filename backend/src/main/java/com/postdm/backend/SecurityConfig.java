797c1e7531546174dec929bfb8f4682b3552a9cb
Etwashoeren <sungmin0609@nate.com>
2025-02-12 23:50:51 +0900

    feat. SecurityConfig에 필터 및 암호화 추가

--- backend/src/main/java/com/postdm/backend/SecurityConfig.java
+++ backend/src/main/java/com/postdm/backend/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.postdm.backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -19,6 +29,9 @@ public class SecurityConfig {
 
         http
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), JwtAuthenticationFilter.class);
 
 
         return http.build();

099efab52397fbac8eb5e0676ab3289a1e4f5929
Etwashoeren <sungmin0609@nate.com>
2025-02-12 13:11:10 +0900

    feat. TokenInfo 작성

---
+++ backend/src/main/java/com/postdm/backend/TokenInfo.java
@@ -0,0 +1,23 @@
+package com.postdm.backend;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TokenInfo {
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private String role;
+
+    @Builder
+    public TokenInfo(String grantType, String accessToken, String refreshToken, String role) {
+        this.grantType = grantType;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.role = role;
+    }
+}

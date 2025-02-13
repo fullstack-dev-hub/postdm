c4fe556b0a3ef83a735d1d9e5d9d75f721169ef3
ella.oh <skfsk1602@gmail.com>
2025-02-13 19:14:33 +0900

    feat:  Spring Boot가 자동으로 데이터소스를 설정하지 않도록 강제

--- backend/src/test/java/com/postdm/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/postdm/backend/BackendApplicationTests.java
@@ -1,9 +1,12 @@
 package com.postdm.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class }) //직접 DB 구축 시, 제거 고려
 class BackendApplicationTests {
 
 	@Test

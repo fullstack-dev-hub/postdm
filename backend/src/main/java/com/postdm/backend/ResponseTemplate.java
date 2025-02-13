8fb0e14b5e8b25d927e3cd27d2a418967272336f
Etwashoeren <sungmin0609@nate.com>
2025-02-13 20:00:26 +0900

    refactor. RestTemplate -> ResponseTemplate로 이름 변경

--- backend/src/main/java/com/postdm/backend/ResponseTemplate.java
+++ backend/src/main/java/com/postdm/backend/ResponseTemplate.java
@@ -2,12 +2,12 @@ package com.postdm.backend;
 
 import org.springframework.http.HttpStatus;
 
-public class RestTemplate<T> {
+public class ResponseTemplate<T> {
     int status;
     String message;
     T data;
 
-    public RestTemplate(HttpStatus status, String message, T data) {
+    public ResponseTemplate(HttpStatus status, String message, T data) {
         this.status = status.value();
         this.message = message;
         this.data = data;

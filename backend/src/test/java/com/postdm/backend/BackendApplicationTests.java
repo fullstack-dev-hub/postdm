c57a3885a40aed8de098b4753bd9a8ccbc9172c3
ella.oh <skfsk1602@gmail.com>
2025-02-18 16:58:49 +0900

    #14 fix: CI/CD 환경에서 application-test.yml을 사용하도록 강제 설정

--- backend/src/test/java/com/postdm/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/postdm/backend/BackendApplicationTests.java
@@ -1,12 +1,11 @@
 package com.postdm.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class }) //직접 DB 구축 시, 제거 고려
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test

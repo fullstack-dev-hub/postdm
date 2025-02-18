a7df3fd48512a4c8b217f9c14954b952bf61394d
ella.oh <skfsk1602@gmail.com>
2025-02-18 16:59:53 +0900

    #14 fix: @Repository 추가

--- backend/src/main/java/com/postdm/backend/domain/member/domain/repository/MemberRepository.java
+++ backend/src/main/java/com/postdm/backend/domain/member/domain/repository/MemberRepository.java
@@ -2,7 +2,9 @@ package com.postdm.backend.domain.member.domain.repository;
 
 import com.postdm.backend.domain.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);

19ff075bc2033b67613f5e7a28367c7ae6685b10
ella.oh <skfsk1602@gmail.com>
2025-02-27 21:55:31 +0900

    #17 fix: JPA에서 필요한 기본 생성자와 서비스 및 테스트 코드에서 필요한 모든 필드 값을 설정하는 생성자 어노테이션 추가

--- backend/src/main/java/com/postdm/backend/domain/estimate/dto/EstimateRequestDto.java
+++ backend/src/main/java/com/postdm/backend/domain/estimate/dto/EstimateRequestDto.java
@@ -2,10 +2,14 @@ package com.postdm.backend.domain.estimate.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Schema(description = "견적서 생성 요청 DTO")
+@NoArgsConstructor // 기본 생성자 추가
+@AllArgsConstructor
 public class EstimateRequestDto {
     @Schema(description = "견적서 내용", example = "이것은 견적서의 내용입니다.")
     @NotBlank(message = "내용은 필수 입력값입니다.")

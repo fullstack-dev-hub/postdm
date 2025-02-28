88310a476b7f17964be4f5a0fb21e848145f9b73
hardlife0 <hellosy76@naver.com>
2025-02-28 15:38:12 +0900

    feature: 로고 클릭시 /home으로 이동(추후 주소변경)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -28,33 +28,35 @@ const Header = () => {
   return (
     <>
       <header className="fixed top-0 left-0 right-0 h-[100px] flex items-center px-5 bg-white z-50">
-        {/* Logo */}
-        <div className="relative w-[42px] h-[40px]">
-          <Image
-            src="/images/logo.svg"
-            alt="Logo"
-            fill
-            className="object-contain"
-            priority
-          />
-        </div>
-        
-        {/* POSTDM */}
-        <div className="relative ml-[15px]">
-          <Image
-            src="/images/POSTDM.svg"
-            alt="POSTDM"
-            width={66}
-            height={24}
-            className="object-contain"
-            style={{
-              fontFamily: 'Impact',
-              fontSize: '20px',
-              lineHeight: '24px',
-              fontWeight: 400,
-            }}
-          />
-        </div>
+        {/* Logo with Link to Home */}
+        <Link href="/home" className="flex items-center">
+          <div className="relative w-[42px] h-[40px]">
+            <Image
+              src="/images/logo.svg"
+              alt="Logo"
+              fill
+              className="object-contain"
+              priority
+            />
+          </div>
+          
+          {/* POSTDM */}
+          <div className="relative ml-[15px]">
+            <Image
+              src="/images/POSTDM.svg"
+              alt="POSTDM"
+              width={66}
+              height={24}
+              className="object-contain"
+              style={{
+                fontFamily: 'Impact',
+                fontSize: '20px',
+                lineHeight: '24px',
+                fontWeight: 400,
+              }}
+            />
+          </div>
+        </Link>
         
         {/* Navigation Button */}
         <button 

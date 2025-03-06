99c86d7bf6340e3bea3a0558e80cd2922a5dfd80
Etwashoeren <sungmin0609@nate.com>
2025-03-06 22:09:33 +0900

    #24 refactor: 이메일 컨트롤러 경로 변경

--- backend/src/main/java/com/postdm/backend/domain/email/api/EmailController.java
+++ backend/src/main/java/com/postdm/backend/domain/email/api/EmailController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/email")
 public class EmailController { // 이메일 관련 컨트롤러
 
     @Autowired

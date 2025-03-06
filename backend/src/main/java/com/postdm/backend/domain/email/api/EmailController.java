cd5db31e7ef338549b1213c5a31379d287539b2e
Etwashoeren <sungmin0609@nate.com>
2025-03-06 22:08:38 +0900

    #24 feat: 비밀번호 재설정 인증 메일 컨트롤러 구현

--- backend/src/main/java/com/postdm/backend/domain/email/api/EmailController.java
+++ backend/src/main/java/com/postdm/backend/domain/email/api/EmailController.java
@@ -25,4 +25,11 @@ public class EmailController { // 이메일 관련 컨트롤러
 
         return new ResponseTemplate<>(HttpStatus.OK, "이메일이 성공적으로 발송되었습니다.", certificationEntity);
     }
+
+    @PostMapping("/reset-certification")
+    public ResponseTemplate<CertificationEntity> resetCertification(@RequestBody @Valid EmailCertificationRequestDto emailCertificationRequestDto) {
+        CertificationEntity certificationEntity = emailService.resetCertification(emailCertificationRequestDto);
+
+        return new ResponseTemplate<>(HttpStatus.OK, "이메일이 성공적으로 발송되었습니다.", certificationEntity);
+    }
 }

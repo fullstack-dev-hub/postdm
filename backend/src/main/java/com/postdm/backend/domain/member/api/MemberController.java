30e124fe7e46d836ce3b23780b0b21cb383bc2b7
Etwashoeren <sungmin0609@nate.com>
2025-03-06 22:11:04 +0900

    #24 feat: 사용자 아이디 찾기 및 비밀번호 재설정 컨트롤러 작성

---
+++ backend/src/main/java/com/postdm/backend/domain/member/api/MemberController.java
@@ -0,0 +1,50 @@
+package com.postdm.backend.domain.member.api;
+
+import com.postdm.backend.domain.email.dto.CheckCertificationRequestDto;
+import com.postdm.backend.domain.member.application.MemberService;
+import com.postdm.backend.domain.member.domain.entity.Member;
+import com.postdm.backend.domain.member.dto.FindUsernameRequestDto;
+import com.postdm.backend.domain.member.dto.ResetPasswordRequestDto;
+import com.postdm.backend.global.template.ResponseTemplate;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/find-userId")
+    public ResponseTemplate<String> findUsername(@RequestBody @Valid FindUsernameRequestDto findUsernameRequestDto) {
+        Member member = memberService.findUsernameByEmail(findUsernameRequestDto);
+
+        String username = member.getUsername();
+
+        String blurred = member.makeBlur(username);
+
+        return new ResponseTemplate<>(HttpStatus.OK, "아이디 찾기 성공", blurred);
+    }
+
+    @PostMapping("/check-certification")
+    public ResponseTemplate<String> checkCertificationNumber(@RequestBody @Valid CheckCertificationRequestDto checkCertificationRequestDto) {
+        String success =  memberService.checkCertificationNumber(checkCertificationRequestDto);
+
+        return new ResponseTemplate<>(HttpStatus.OK, success, "****");
+    }
+
+    @PostMapping("/reset-password")
+    public ResponseTemplate<String> resetPassword(@RequestBody @Valid ResetPasswordRequestDto requestDto) {
+        String success = memberService.resetPassword(requestDto);
+
+        return new ResponseTemplate<>(HttpStatus.OK, success, "****");
+    }
+}

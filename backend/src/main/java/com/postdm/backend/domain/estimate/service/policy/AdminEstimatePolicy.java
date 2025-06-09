e3dc1260bc53607a000736f21cfa2383fd369ee3
ella.oh <skfsk1602@gmail.com>
2025-06-10 00:30:13 +0900

    #68 feat: 사용자 및 관리자 견적서 조회에 Page<Estimate> 반환 방식 적용

--- backend/src/main/java/com/postdm/backend/domain/estimate/service/policy/AdminEstimatePolicy.java
+++ backend/src/main/java/com/postdm/backend/domain/estimate/service/policy/AdminEstimatePolicy.java
@@ -3,14 +3,17 @@ package com.postdm.backend.domain.estimate.service.policy;
 import com.postdm.backend.domain.estimate.entity.Estimate;
 import com.postdm.backend.domain.estimate.entity.EstimateRepository;
 import com.postdm.backend.domain.member.domain.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 @Component
+@RequiredArgsConstructor
 public class AdminEstimatePolicy implements EstimatePolicy {
     @Override
-    public List<Estimate> getEstimates(Member member, EstimateRepository repository) {
-        return repository.findAll(); // 관리자: 전체 조회
+    public Page<Estimate> getEstimates(Member member, EstimateRepository repository, Pageable pageable) {
+        return repository.findAll(pageable);
     }
 }

8c5d21c61f4ffe8b53ba1fd47fb00240e95abdde
ella.oh <skfsk1602@gmail.com>
2025-06-13 14:23:59 +0900

    #68 refactor: 커스텀 페이지 응답 DTO 재구성 및 정렬 순서, 기준 추가

--- backend/src/main/java/com/postdm/backend/domain/estimate/dto/PageResponse.java
+++ backend/src/main/java/com/postdm/backend/domain/estimate/dto/PageResponse.java
@@ -1,14 +1,16 @@
 package com.postdm.backend.domain.estimate.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@NoArgsConstructor
-public class PageResponse<T>  {
+@AllArgsConstructor
+public class PageResponse<T> {
     private List<T> content;
     private int page;
     private int size;
@@ -16,16 +18,34 @@ public class PageResponse<T>  {
     private int totalPages;
     private boolean first;
     private boolean last;
-    private boolean sorted;
+    private List<SortInfo> sort;
 
-    public PageResponse(Page<T> page) {
-        this.content = page.getContent();
-        this.page = page.getNumber();
-        this.size = page.getSize();
-        this.totalElements = page.getTotalElements();
-        this.totalPages = page.getTotalPages();
-        this.first = page.isFirst();
-        this.last = page.isLast();
-        this.sorted = page.getSort().isSorted();
+    public static <T> PageResponse<T> from(Page<T> page) {
+        List<SortInfo> sortInfo = page.getSort().stream()
+                .map(SortInfo::new)
+                .collect(Collectors.toList());
+
+        return new PageResponse<>(
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isFirst(),
+                page.isLast(),
+                sortInfo
+        );
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class SortInfo {
+        private String property;
+        private String direction;
+
+        public SortInfo(Sort.Order order) {
+            this.property = order.getProperty();
+            this.direction = order.getDirection().name();
+        }
     }
 }
